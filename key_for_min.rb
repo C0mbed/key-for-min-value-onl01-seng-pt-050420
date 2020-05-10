788445265dfeba7e661fb691f118c3614ecae671
Matt Farncombe <matt@farncombe.co>
2020-05-10 17:28:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_value = 1000
   lowest_key = ""
-  if name_hash == nil
+  if name_hash.length == 0
     return nil
   else
     name_hash.each do |key, value|
